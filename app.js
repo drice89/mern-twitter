c12ae6cedadb464210c916dbe25d639936bc5cc2
drice89 <drice89@gmail.com>
2020-05-09 23:58:03 -0700

    Add everything i have done thus far

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User')
+const bodyParser = require('body-parser');
+const passport = require("passport")
+
+
+const port = process.env.PORT || 5000;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(bodyParser.json());
+app.user(passport.initialize());
+require("./config/passport")(passport);
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
