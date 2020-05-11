e7098353808b21cdac85070054af4b26a9b57f6e
drice89 <drice89@gmail.com>
2020-05-10 23:03:02 -0700

    Add prod env info

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express"); 
 const app = express(); 
 const db = require('./config/keys').mongoURI;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const passport = require('passport')
+const passport = require('passport');
 
-const users = require('./routes/api/users')
-const tweets = require('./routes/api/tweets')
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+const path = require('path');
 
 mongoose
 .connect(db, {useNewUrlParser: true})
@@ -22,5 +23,12 @@ app.use(bodyParser.json());
 app.use('/api/users', users)
 app.use('/api/tweets', tweets)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const port = process.env.PORT || 5000; 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
